5d79510d684164d9bc8b0ecd6fc3aff32352fde9
Ruona125 <ogheneruonaagadagba4@gmail.com>
2025-06-21 00:04:36 +0100

    added the services for warehouse and also the warehouse update dto

--- obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48cc0d03af00a07c0925ebde8a97f1a879c0f557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93253a4f77e1b6594fbab805eff05b703962ca3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
