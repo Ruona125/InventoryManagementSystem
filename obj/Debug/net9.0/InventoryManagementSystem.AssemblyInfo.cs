934b39cb2845ef1fb46848c2d250ec387855dd5a
Ruona125 <ogheneruonaagadagba4@gmail.com>
2025-06-20 20:15:42 +0100

    created the product controller

--- obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f57b64f554e7c0b5ed8a1019be3d3b47bddfd35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ca0864f99c9bedaf47e4d0c0d3cf97088fba98")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
