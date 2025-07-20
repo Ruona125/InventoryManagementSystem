718aeee64711b0e0a46aa74e6acf8f80500f69eb
Ruona125 <ogheneruonaagadagba4@gmail.com>
2025-07-20 22:47:26 +0100

    Add ECS GitHub Actions CI/CD pipeline

--- obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759312495e09bf2847ea05e5dddaa18728fd462f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fdc2a301bbcda63a7346eadad25d0f1aea04e4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
